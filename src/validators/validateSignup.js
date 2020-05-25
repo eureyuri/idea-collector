ad85bacd790b069e1e3996b3c5e3bccaf91a6841
eureyuri <eureynoguchi@gmail.com>
2020-05-25 10:39:28 -0400

    Authentication pages with Firebase

---
+++ src/validators/validateSignup.js
@@ -0,0 +1,21 @@
+export default function validateSignup(values) {
+  let errors = {};
+
+  if (!values.name) {
+    errors.name = "A username is required";
+  }
+
+  if (!values.email) {
+    errors.email = "A email is required";
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+    errors.email = "Email is invalid";
+  }
+
+  if (!values.password) {
+    errors.password = "A password is required";
+  } else if (values.password.length < 6) {
+    errors.password = "Password must be at least 6 characters";
+  }
+
+  return errors;
+}
