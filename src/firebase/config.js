23135933e6c22f70358182c84a1f96b9c12901c1
eureyuri <eureynoguchi@gmail.com>
2020-05-24 21:48:00 -0400

    Firebase setup

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDqOCUQd_3EGVTA_w9PubRbKN8o4tVC51k",
+  authDomain: "idea-collector-bb9fc.firebaseapp.com",
+  databaseURL: "https://idea-collector-bb9fc.firebaseio.com",
+  projectId: "idea-collector-bb9fc",
+  storageBucket: "idea-collector-bb9fc.appspot.com",
+  messagingSenderId: "296507533998",
+  appId: "1:296507533998:web:42d5f61bb88d4bc35e9b3f",
+  measurementId: "G-HMPBE49Y60"
+};
+
+export default firebaseConfig;
