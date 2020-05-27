08463ca9b221066f2baa834ed418d5505cea32e3
eureyuri <eureynoguchi@gmail.com>
2020-05-27 10:44:50 -0400

    style

--- src/components/Header/SmallHeader.js
+++ src/components/Header/SmallHeader.js
@@ -13,14 +13,14 @@ const LargeHeader = ({title}) => {
               <IonTitle size="large">{title}</IonTitle>
             </IonCol>
             <IonCol text-center>
-            <div class="title">
-              <IonTitle size="large" text-right>
-                <a href="https://www.youtube.com/channel/UCOScmlJ9J2xpLwuOYQu4n9Q" target="_blank">
-                ツレアム　
-                <i class="fa fa-youtube-play"></i>
-                </a>
-              </IonTitle>
-            </div>
+              <div class="title">
+                <IonTitle size="large" text-right>
+                  <a href="https://www.youtube.com/channel/UCOScmlJ9J2xpLwuOYQu4n9Q" target="_blank">
+                  ツレアム　
+                  <i class="fa fa-youtube-play"></i>
+                  </a>
+                </IonTitle>
+              </div>
             </IonCol>
           </IonRow>
         </IonGrid>
